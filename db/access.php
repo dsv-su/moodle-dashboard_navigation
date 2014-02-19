8f539fd1b5e1862b7875eaf9ce173111f76f7e94
papy123 <papybp@gmail.com>
2014-02-19 11:05:43 +0200

    adding a new db folder

---
+++ db/access.php
@@ -0,0 +1,26 @@
+<?php
+    $capabilities = array(
+ 
+    'block/dashboard_navigation:myaddinstance' => array(
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_SYSTEM,
+        'archetypes' => array(
+            'user' => CAP_ALLOW
+        ),
+ 
+        'clonepermissionsfrom' => 'moodle/my:manageblocks'
+    ),
+ 
+    'block/dashboard_navigation:addinstance' => array(
+        'riskbitmask' => RISK_SPAM | RISK_XSS,
+ 
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_BLOCK,
+        'archetypes' => array(
+            'editingteacher' => CAP_ALLOW,
+            'manager' => CAP_ALLOW
+        ),
+ 
+        'clonepermissionsfrom' => 'moodle/site:manageblocks'
+    ),
+);
