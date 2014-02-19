751d56b7ad4321d5c9ae4d6cd9c5267819e5f703
papy123 <papybp@gmail.com>
2014-02-19 11:02:45 +0200

    started writing on dashboard

---
+++ block_dashboard_navigation.php
@@ -0,0 +1,7 @@
+<?php
+class block_dashboard_navigation extends block_base {
+    public function init() {
+        $this->title = get_string('dashboard_navigation', 'block_dashboard_navigation');
+    }
+    // The PHP tag and the curly bracket for the class definition 
+    // will only be closed after there is another function added in the next section.
